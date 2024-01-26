ae08113c0ef7275e8d142c88b02e1e58371a472f
James Hawk <jph94880@gmail.com>
2024-01-26 09:16:28 -0600

    And useEffect and useState to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, createContext, useState } from 'react';
 
 export const movies = createContext(movies)
 
 
 function App() {
+ const [movies, setMovies] = useState()
+
+  useEffect(() => {
+    fetch('http://localhost:5555/movies')
+    .then((r) => r.json())
+    .then(setMovies)
+  })
+
   return (
     <movies.Provider movies={movies}>
     <MoviesPage/>
